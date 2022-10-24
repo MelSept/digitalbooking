02789b4a41b994aaabb2edc34491b49ad82a989b
Juan Camilo Hernandez Ramirez <juancamilo.2001.hr@gmail.com>
2022-10-23 19:47:18 -0500

    test(backend-category): Add tests to the category service layer

---
+++ backend/API-Lodgings/src/test/java/com/digitalbooking/apilodgings/ApiLodgingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalbooking.apilodgings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLodgingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
