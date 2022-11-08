6fe006ddb9e85614c9ae8712ddd1caaa60793b1c
Juan Camilo Hernandez Ramirez <juancamilo.2001.hr@gmail.com>
2022-11-07 20:29:13 -0500

    refactor(backend-category): Refactor code structure for pom and application.properties

--- backend/API-Lodgings/src/main/java/com/digitalbooking/apilodgings/dto/CategoryDTO.java
+++ backend/API-Lodgings/src/main/java/com/digitalbooking/apilodgings/dto/CategoryDTO.java
@@ -6,18 +6,26 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
+/**
+ * A DTO for the {@link com.digitalbooking.apilodgings.entity.Category} entity
+ */
 @Getter
 @Setter
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Data
-public class CategoryDTO {
+public class CategoryDTO implements Serializable {
+
     @NotNull(message = "The 'id' field cannot be null.")
-    public Integer id;
+    private Integer id;
+
     @NotNull(message = "The 'title' field cannot be null.")
-    public String title;
+    private String title;
+
     @NotNull(message = "The 'description' field cannot be null.")
-    public String description;
+    private String description;
+
     @NotNull(message = "The 'imageUrl' field cannot be null.")
-    public String imageUrl;
+    private String imageUrl;
 }
